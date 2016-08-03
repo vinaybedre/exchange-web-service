12385ea390f183798255f905da3cef41f2863a53
Vinay Bedre <vinaybedre@gmail.com>
2016-08-03 14:14:16 +0530

    create appointment
    create appointment with subject, body, start and end date, status and
    location.

--- index.js
+++ index.js
@@ -8,18 +8,26 @@ var builder = new xml2js.Builder();
 
 var ews = function() {
     that = this;
+    that.constants = {
+        CalendarBusyStatus: {
+            Free: 'Free',
+            Tentative: 'Tentative',
+            Busy: 'Busy',
+            OutOfOffice: 'OOF',
+            NoStatus: 'NoData',
+            WorkingElsewhere: 'WorkingElsewhere'
+        }
+    };
+
     that.configObj = {};
     that.config = function(username, password, url, domain) {
         that.configObj.username = username;
         that.configObj.password = password;
         that.configObj.url = url;
-        this.configObj.domain = domain;
+        that.configObj.domain = domain;
     };
 
-    this.createTask = function(subject, dueDate) {
-        var contents = fs.readFileSync(__dirname + '/xmlFiles/createTask.xml').toString();
-        contents = contents.replace("_SUBJECT_", subject);
-        contents = contents.replace("_DUE_DATE_", dueDate);
+    that.post = function(contents) {
         httpntlm.post({
             url: that.configObj.url,
             username: that.configObj.username,
@@ -35,38 +43,38 @@ var ews = function() {
             if (res.statusCode != 200) {
                 console.log('Error with status code: ' + res.statusCode);
             } else {
-                console.log("Task Created");
+                console.log("Success");
             }
+            // parseString(res.body, function(err, res) {
+            //     console.log(res['s:Envelope']['s:Body'][0]['m:CreateItemResponse'][0]);
+            // });
         });
+    }
+
+    this.createTask = function(subject, dueDate) {
+        var contents = fs.readFileSync(__dirname + '/xmlFiles/createTask.xml').toString();
+        contents = contents.replace("_SUBJECT_", subject);
+        contents = contents.replace("_DUE_DATE_", dueDate);
+        that.post(contents);
     };
+
+    this.createAppointment = function(subject, body, startTime, endTime, busyStatus, location) {
+        var contents = fs.readFileSync(__dirname + '/xmlFiles/createAppointment.xml').toString();
+        contents = contents.replace("_SUBJECT_", subject);
+        contents = contents.replace("_BODY_", body);
+        contents = contents.replace("_START_DATE_", startTime);
+        contents = contents.replace("_END_DATE_", endTime);
+        contents = contents.replace("_STATUS_", busyStatus);
+        contents = contents.replace("_LOCATION_", location);
+        that.post(contents);
+    }
 
     this.sendMail = function(csvTo, subject, body) {
         var contents = fs.readFileSync(__dirname + '/xmlFiles/sendMail.xml').toString();
         contents = contents.replace("_TO_", csvTo);
         contents = contents.replace("_SUBJECT_", subject);
         contents = contents.replace("_BODY_", body);
-
-        httpntlm.post({
-            url: that.configObj.url,
-            username: that.configObj.username,
-            password: that.configObj.password,
-            domain: that.configObj.domain,
-            body: contents,
-            headers: {
-                "content-type": "text/xml",
-                "cache-control": "no-cache"
-            }
-        }, function(err, res) {
-            if (err) return err;
-            if (res.statusCode != 200) {
-                console.log('Error with status code: ' + res.statusCode);
-            } else {
-                console.log("Mail Sent");
-            }
-            // parseString(res.body, function(err, res) {
-            //     console.log(res['s:Envelope']['s:Body'][0]['m:CreateItemResponse'][0]);
-            // });
-        });
+        that.post(contents);
     }
 }
 
